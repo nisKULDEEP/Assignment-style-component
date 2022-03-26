0df0551d5d8a5e7513d80bc7a7af1082e0fb6363
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-03-26 18:49:09 +0530

    button style added

---
+++ src/Components/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "#4278f5" : "#ffffff")};
+  border: ${(props) =>
+    props.default
+      ? "0.5px solid #c2c2c2"
+      : props.dashed
+      ? "1px dashed #c2c2c2"
+      : "none"};
+  color: ${(props) =>
+    props.primary ? "#ffffff" : props.link ? "#4278f5" : "black"};
+  border-radius: 2px;
+
+  padding: 5px 15px;
+  font-size: 20px;
+  margin: 10px;
+  &:hover {
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    cursor: pointer;
+  }
+`;
+
+// function Button() {
+//   return (
+//     <buttonWrapper>
+//        <button>HELLO</button>
+//     </buttonWrapper>
+//   )
+// }
+
+export default Button;
